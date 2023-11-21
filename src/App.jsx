a2d228f6e385cfdd8e06fe854042a60bddbaf8d5
Sina-Soroosh <sinasoroosh07@gmail.com>
2023-11-21 20:38:02 +0330

    add react-router-dom in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
+import { useRoutes } from "react-router-dom";
+import routes from "./routes";
 
 function App() {
+  const router = useRoutes(routes);
+
   return (
     <>
       <Header />
+      {router}
     </>
   );
 }
