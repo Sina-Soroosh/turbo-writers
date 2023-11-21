aafe9f6f41be43d41bca01cd9edd27fbadbbe922
Sina-Soroosh <sinasoroosh07@gmail.com>
2023-11-21 21:55:27 +0330

    Coding ArticleBoxIndex component

--- src/components/ArticleBoxIndex/ArticleBoxIndex.jsx
+++ src/components/ArticleBoxIndex/ArticleBoxIndex.jsx
@@ -2,7 +2,26 @@ import React from "react";
 import "./ArticleBoxIndex.css";
 
 function ArticleBoxIndex() {
-  return <div>ArticleBoxIndex</div>;
+  return (
+    <div className="col-lg-4 col-sm-6 col-12">
+      <div className="content-article-box-index">
+        <div className="image-article">
+          <img
+            src="https://sabzlearn.ir/wp-content/uploads/2023/08/Com_python-768x432.png"
+            alt=""
+          />
+        </div>
+        <div className="name-article">
+          <a href="">
+            <h3>آموزش پایتون رایگان مقدماتی تا پیشرفته + پروژه های جذاب</h3>
+          </a>
+        </div>
+        <div className="date-article">
+          <p>19 خرداد 1402</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ArticleBoxIndex;
--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./Index.css";
+import ArticleBoxIndex from "../../components/ArticleBoxIndex/ArticleBoxIndex";
 
 function Home() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="content-index">
+        <div className="new-articles">
+          <div className="content-new-articles">
+            <div className="row">
+              <ArticleBoxIndex />
+              <ArticleBoxIndex />
+              <ArticleBoxIndex />
+              <ArticleBoxIndex />
+              <ArticleBoxIndex />
+              <ArticleBoxIndex />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
