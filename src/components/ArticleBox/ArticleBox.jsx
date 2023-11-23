f04fefb0995384cbd4e84a625b1fbe5c3e16b438
Sina-Soroosh <sinasoroosh07@gmail.com>
2023-11-23 17:03:03 +0330

    Coding ArticleBox component

--- src/components/ArticleBox/ArticleBox.jsx
+++ src/components/ArticleBox/ArticleBox.jsx
@@ -2,7 +2,63 @@ import React from "react";
 import "./ArticleBox.css";
 
 function ArticleBox() {
-  return <div>ArticleBox</div>;
+  return (
+    <div className="col-lg-4 col-sm-6 col-12">
+      <div className="article-box">
+        <div className="content-article-box">
+          <div className="image-article-box">
+            <a href="">
+              <img
+                src="https://demos.turboweb-wp.ir/topnevisan/wp-content/uploads/2023/08/sabz-next-410x230.png"
+                alt=""
+              />
+            </a>
+          </div>
+          <div className="title-article-box">
+            <a href="">
+              <h3>
+                یادگیری ری اکت ( ReactJS ) در دنیای واقعی | از 0 تا استخدام
+                [منتورشیپ]
+              </h3>
+            </a>
+          </div>
+          <div className="category-article-box">
+            <a href="">
+              <span className="category">برنامه نویسی</span>
+            </a>
+          </div>
+          <div className="desc-article-box">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repudiandae non suscipit repellendus, perspiciatis neque soluta
+              laboriosam nam velit ipsam eum ipsa optio officia a, qui doloribus
+              maxime, itaque architecto alias. Obcaecati a natus numquam quia ad
+              delectus adipisci ab fugiat. Molestias itaque quas qui molestiae
+              magnam minima rerum deserunt nemo. Voluptates explicabo
+              accusantium fuga, itaque, ab ullam ducimus dolores provident
+              incidunt temporibus beatae laudantium, dicta dignissimos
+              obcaecati? Modi porro recusandae, eos, nostrum quas eveniet
+              mollitia eum totam temporibus, dolores placeat ea earum
+              consequuntur quisquam eaque officia provident nisi inventore
+              similique nulla error itaque? Odit placeat dolorum ipsum obcaecati
+              quas nam magnam velit, molestiae in expedita voluptate fuga
+              provident sit, aut consequatur enim temporibus. Accusamus culpa
+              asperiores id quis voluptates accusantium veniam unde nemo non!
+              Magni odit quas quae aut officiis, illum eos fuga ducimus amet
+              nobis delectus laboriosam quod debitis perferendis fugit
+              exercitationem aperiam commodi nulla. Dignissimos omnis adipisci
+              beatae?
+            </p>
+          </div>
+          <div className="read-article-box">
+            <a href="">
+              <span>مطالعه</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ArticleBox;
--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./Articles.css";
 import BoxArticles from "../../components/BoxArticles/BoxArticles";
+import ArticleBox from "../../components/ArticleBox/ArticleBox";
 
 function Articles() {
   return (
     <div className="articles">
-      <BoxArticles title="کلیه مقالات :"></BoxArticles>
+      <BoxArticles title="کلیه مقالات :">
+        <ArticleBox />
+        <ArticleBox />
+        <ArticleBox />
+        <ArticleBox />
+        <ArticleBox />
+        <ArticleBox />
+      </BoxArticles>
     </div>
   );
 }
