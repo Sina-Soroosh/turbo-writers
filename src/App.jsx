44b9270a94bcc2a6ee02bdec16533e4f0e7279a7
Sina-Soroosh <sinasoroosh07@gmail.com>
2023-11-24 11:13:19 +0330

    Fix bug change route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
 import Header from "./components/Header/Header";
-import { useRoutes } from "react-router-dom";
+import { useLocation, useRoutes } from "react-router-dom";
 import router from "./router.jsx";
 import Footer from "./components/Footer/Footer";
+import { useEffect } from "react";
 
 function App() {
   const routes = useRoutes(router);
+  const url = useLocation();
+
+  useEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, [url]);
 
   return (
     <>
