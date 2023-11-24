87e8198a5269c21ac2364753e2df7a2b9c88b515
Sina-Soroosh <sinasoroosh07@gmail.com>
2023-11-24 10:55:34 +0330

    Finish Articles page

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -5,28 +5,38 @@ import ArticleBox from "../../components/ArticleBox/ArticleBox";
 import { useDispatch, useSelector } from "react-redux";
 import { getArticles } from "../../redux/store/articles";
 import { Pagination } from "@mui/material";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 function Articles() {
   const dispatch = useDispatch();
-  const articles = useSelector((state) => state.articles);
+  const { articles, count } = useSelector((state) => state.articles);
+  const { page } = useParams();
+  const Location = useLocation();
+  const navigate = useNavigate();
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    dispatch(getArticles({ page: page || 1, manyShow: 6 }));
+  }, [Location]);
+
+  const changePageHandler = (e, page) => {
+    navigate(`/articles/${page}`);
+  };
 
   return (
     <div className="articles">
       <BoxArticles title="کلیه مقالات :">
-        <ArticleBox />
-        <ArticleBox />
-        <ArticleBox />
-        <ArticleBox />
-        <ArticleBox />
-        <ArticleBox />
+        {articles?.map((article) => (
+          <ArticleBox key={article.id} {...article} />
+        ))}
+        {console.log(page)}
         <div className="pagination">
           <Pagination
-            count={10}
+            count={count}
             variant="outlined"
             shape="rounded"
             size="large"
+            page={+page || 1}
+            onChange={changePageHandler}
           />
         </div>
       </BoxArticles>
--- src/redux/store/articles.js
+++ src/redux/store/articles.js
@@ -8,7 +8,10 @@ const slice = createSlice({
     getArticles: (state, action) => {
       const lastIndex = action.payload.page * action.payload.manyShow;
       const firstIndex = lastIndex - action.payload.manyShow;
-      return data.articles.slice(firstIndex, lastIndex);
+      return {
+        articles: data.articles.slice(firstIndex, lastIndex),
+        count: Math.ceil(data.articles.length / action.payload.manyShow),
+      };
     },
   },
 });
