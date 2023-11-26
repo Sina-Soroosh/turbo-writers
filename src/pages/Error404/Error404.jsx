a74f18c5ffd3b4643e3d35618d941685911aa7d1
Sina-Soroosh <sinasoroosh07@gmail.com>
2023-11-26 19:50:12 +0330

    Coding Error404 page

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import "./Error404.css";
+import { Link } from "react-router-dom";
 
 function Error404() {
-  return <div>Error404</div>;
+  return (
+    <div className="err-404">
+      <div className="container">
+        <div className="row content-err-404">
+          <div className="col-lg-7">
+            <div className="title-err-404">
+              <h5>
+                ممکن است صفحه ای که به دنبال آن میگردید حذف شده باشد و یا آدرس
+                آن را به درستی وارد نکرده باشید
+              </h5>
+            </div>
+            <Link to="/">بازگشت به صفحه اصلی</Link>
+          </div>
+          <div className="col-lg-5">
+            <div className="image-err-400">
+              <img src="/public/image/404.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Error404;
